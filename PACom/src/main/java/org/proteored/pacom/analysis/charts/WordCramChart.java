afe300ec989e3c30280b8bd22a3f56febe62a9e9
Salvador Martinez-Bartolome <salvador@scripps.edu>
2017-08-01 17:21:35 -0700

    minor update

--- PACom/src/main/java/org/proteored/pacom/analysis/charts/WordCramChart.java
+++ PACom/src/main/java/org/proteored/pacom/analysis/charts/WordCramChart.java
@@ -1,17 +1,5 @@
 package org.proteored.pacom.analysis.charts;
 
-/*
- * Copyright 2010 Daniel Bernier Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
-import java.awt.GridBagConstraints;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +7,6 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import org.apache.log4j.Logger;
@@ -41,12 +28,16 @@ import wordcram.WordCounter;
 import wordcram.WordCram;
 
 public class WordCramChart extends PApplet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4348652276715910780L;
+
 	private static Logger log = Logger.getLogger("log4j.logger.org.proteored");
 
 	private WordCram wordcram;
 	private final Word[] words;
 	private static List<String> wordsToSkip = new ArrayList<String>();
-	private int minWordLength = 4;
 	private JLabel jLabelSelectedWord;
 	private static String noGoodFontNames = "Dingbats|Standard Symbols L";
 	private static String blockFontNames = "OpenSymbol|Mallige Bold|Mallige Normal|Lohit Bengali|Lohit Punjabi|Webdings";
@@ -55,10 +46,6 @@ public class WordCramChart extends PApplet {
 	private final boolean mapWordsAndProteins = true;
 
 	private JTextArea jTextAreaSelectedProteins;
-
-	private final JPanel panel = new JPanel();
-
-	private GridBagConstraints c;
 
 	private String selectedFont;
 
@@ -230,11 +217,6 @@ public class WordCramChart extends PApplet {
 
 	public WordCramChart maximumNumberOfWords(int number) {
 		maximumNumberOfWords = number;
-		return this;
-	}
-
-	public WordCramChart minWordLength(int minLength) {
-		minWordLength = minLength;
 		return this;
 	}
 
