507aa92f1828d68be54f9bfdf32adf112de715ac
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-06-01 12:09:28 -0700

    check every 5 min

--- PACom/src/main/java/org/proteored/pacom/gui/tasks/CheckUpdateTask.java
+++ PACom/src/main/java/org/proteored/pacom/gui/tasks/CheckUpdateTask.java
@@ -4,12 +4,21 @@ import javax.swing.SwingWorker;
 
 import org.proteored.pacom.utils.UpdateChecker;
 
+import edu.scripps.yates.utilities.dates.DatesUtil;
+
 public class CheckUpdateTask extends SwingWorker<Void, String> {
-	private static org.apache.log4j.Logger log = org.apache.log4j.Logger
-			.getLogger("log4j.logger.org.proteored");
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger("log4j.logger.org.proteored");
+	private final long delay;
 
+	/**
+	 * Check update thread. Checking by default every 5 min
+	 */
 	public CheckUpdateTask() {
+		delay = 1000 * 60 * 5;// 5 min
+	}
 
+	public CheckUpdateTask(long delayInMillisenconds) {
+		delay = delayInMillisenconds;
 	}
 
 	@Override
@@ -17,14 +26,14 @@ public class CheckUpdateTask extends SwingWorker<Void, String> {
 
 		try {
 			while (true) {
-				log.info("Checking for an update");
+				log.info("Checking for an update in PACOM...");
 				if (UpdateChecker.getInstance().hasUpdate()) {
 					log.info("Update found");
 					UpdateChecker.getInstance().showUpdateDialog();
 				} else {
-					log.info("Nothing to update");
+					log.info("Nothing to update. NExt check in " + DatesUtil.getDescriptiveTimeFromMillisecs(delay));
 				}
-				Thread.sleep(1000 * 60 * 30); // 30 min
+				Thread.sleep(delay);
 			}
 		} finally {
 		}
